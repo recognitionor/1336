ea2c7585047d4474febabf61606eea035702367e
junghee <junghee@neowiz.com>
2022-08-03 17:34:36 +0900

    소셜 로그인

--- app/src/main/java/com/ham/onettsix/LoginActivity.kt
+++ app/src/main/java/com/ham/onettsix/LoginActivity.kt
@@ -1,6 +1,16 @@
 package com.ham.onettsix
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.toolbar_back.view.*
 
 class LoginActivity : AppCompatActivity(R.layout.activity_login) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        login_toolbar_back.back_btn.setOnClickListener {
+            finish()
+        }
+    }
 }
